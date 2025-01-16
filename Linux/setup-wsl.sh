b4c05b3636c2dd978da43d93ca990f5ec9c0fd41
Paul Jiang <zhaojia@microsoft.com>
2025-01-16 14:39:50 +0800

    Fix

--- Linux/setup-wsl.sh
+++ Linux/setup-wsl.sh
@@ -1,17 +1,17 @@
-# install python
-sudo apt update -y
-sudo apt install -y python3
-sudo apt install -y python-is-python3
-sudo apt install -y python3-pip
+# Install Python
+sudo DEBIAN_FRONTEND=noninteractive apt update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install -y python3
+sudo DEBIAN_FRONTEND=noninteractive apt install -y python-is-python3
+sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
 
-# install docker and docker-compose
-sudo apt update -y
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+# Install Docker and Docker Compose
+sudo DEBIAN_FRONTEND=noninteractive apt update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update -y
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo DEBIAN_FRONTEND=noninteractive apt update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chmod 666 /var/run/docker.sock
