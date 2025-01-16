1313fcb866eae702d2c0684e4046f5bd1690492a
Paul Jiang <zhaojia@microsoft.com>
2025-01-16 14:44:02 +0800

    add install git

--- Linux/setup-wsl.sh
+++ Linux/setup-wsl.sh
@@ -15,3 +15,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y docker-ce docker-ce-cli conta
 sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chmod 666 /var/run/docker.sock
+
+# Install Git
+sudo DEBIAN_FRONTEND=noninteractive apt update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install -y git
