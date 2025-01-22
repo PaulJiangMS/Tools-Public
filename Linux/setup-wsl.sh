30724c7c6cd6093d0910fe3c9ffd4793fb0d8367
Paul Jiang <zhaojia@microsoft.com>
2025-01-22 16:56:41 +0800

    fix

--- Linux/setup-wsl.sh
+++ Linux/setup-wsl.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
 # Install Python
+echo "Installing Python..."
 sudo DEBIAN_FRONTEND=noninteractive apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install -y python3
 sudo DEBIAN_FRONTEND=noninteractive apt install -y python-is-python3
 sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip
 
 # Install Docker and Docker Compose
+echo "Installing Docker and Docker Compose..."
 sudo DEBIAN_FRONTEND=noninteractive apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -12,26 +16,34 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 
 sudo DEBIAN_FRONTEND=noninteractive apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install -y docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chmod 666 /var/run/docker.sock
 
 # Install Git
+echo "Installing Git..."
 sudo DEBIAN_FRONTEND=noninteractive apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install -y git
 
 # Install jq
+echo "Installing jq..."
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y jq
 
-# Install conda
+# Install Miniconda
+echo "Installing Miniconda..."
 mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 source ~/miniconda3/bin/activate
-conda init --all
+~/miniconda3/bin/conda init --all
 
 # Install gnome-terminal
+echo "Installing gnome-terminal..."
 sudo DEBIAN_FRONTEND=noninteractive apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install -y gnome-terminal
+
+echo "Installation complete!"
