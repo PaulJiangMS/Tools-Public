683500b919d68cb6b74ead8da0367e2919ae1029
Paul Jiang <zhaojia@microsoft.com>
2025-02-28 00:52:57 -0800

    Fix

---
+++ Windows/install.ps1
@@ -0,0 +1,14 @@
+echo "Install Git..."
+winget install --id Git.Git -e --source winget
+
+echo "Install Visual Studio Code..."
+winget install --id Microsoft.VisualStudioCode -e --source winget
+
+echo "Install PowerToys..."
+winget install --id Microsoft.PowerToys -e --source winget
+
+echo "Install Snipast..."
+winget install --id Snipaste.Snipaste -e --source winget
+
+echo "Install Bing Wallpaper..."
+winget install --id Microsoft.BingWallpaper -e --source winget
